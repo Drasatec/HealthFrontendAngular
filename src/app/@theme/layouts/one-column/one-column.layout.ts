5ac42a2ba01fccc4710d365ac488eb7a1609ed9c
MaiYakout <70839650+MaiYakout@users.noreply.github.com>
2023-05-19 23:44:08 +0300

    sidemenu

--- src/app/@theme/layouts/one-column/one-column.layout.ts
+++ src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { NbMenuService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-one-column-layout',
   styleUrls: ['./one-column.layout.scss'],
   template: `
     <nb-layout windowMode>
-      <nb-layout-header fixed>
+      <nb-layout-header fixed subheader>
         <ngx-header></ngx-header>
       </nb-layout-header>
 
       <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive start>
+        <a class="logo" href="#" (click)="navigateHome()">مستشفي الرحمة</a>
         <ng-content select="nb-menu"></ng-content>
       </nb-sidebar>
 
@@ -23,4 +25,12 @@ import { Component } from '@angular/core';
     </nb-layout>
   `,
 })
-export class OneColumnLayoutComponent {}
+export class OneColumnLayoutComponent {
+  constructor(
+  private menuService: NbMenuService,
+  ){}
+  navigateHome() {
+    this.menuService.navigateHome();
+    return false;
+  }
+}
