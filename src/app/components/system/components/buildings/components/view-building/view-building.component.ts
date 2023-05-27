68e72e94e4884063e3580e690925569925d1f211
MaiYakout <70839650+MaiYakout@users.noreply.github.com>
2023-05-28 01:26:06 +0300

    hospital

--- src/app/components/system/components/buildings/components/view-building/view-building.component.ts
+++ src/app/components/system/components/buildings/components/view-building/view-building.component.ts
@@ -10,6 +10,7 @@ export class ViewBuildingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log("")
   }
 
 }
--- src/app/components/system/components/hospitals/hospitals.component.ts
+++ src/app/components/system/components/hospitals/hospitals.component.ts
@@ -10,6 +10,8 @@ export class HospitalsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log("")
+
   }
 
 }
